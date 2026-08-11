494d34506cc398f5040f9fa4e24ee884b29f35ab
LuisFelipe <luisfexd14@gmail.com>
2026-08-11 14:53:01 -0400

    Realizado ejercicio 2.11

--- parte2/phonebook/src/App.jsx
+++ parte2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, handleFilterChange }) => {
     return (
@@ -36,20 +37,24 @@ const Persons = ({ persons }) => {
     }
 }
 
-
 const Contact = ({ person }) => <p>{person.name} {person.number}</p>
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ filter, setFilter ] = useState('')
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('ya en ejecucion el useEffect y el controlador de eventos de then')
+                setPersons(response.data)
+                console.log('xd')
+            })
 
+    }, [])
+    console.log('hola')
     const handleNameChange = (event) => setNewName(event.target.value)
 
     const handleNumberChange = event => setNewNumber(event.target.value)
@@ -71,8 +76,6 @@ const App = () => {
 
     const contactToShow = persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
     
-    console.log(persons)
-
     return (
         <div>
             <h2>Phonebook</h2>
